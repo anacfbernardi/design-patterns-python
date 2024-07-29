304b7375a07fcfdeb2569b4f57034ed3266aee72
Ana Bernardi <anacfbernardi@gmail.com>
2024-07-29 18:09:16 -0300

    fix names and functions

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from src.creational_patterns.abstract_factory.abstract_factory import factory_method
+from src.creational_patterns.abstract_factory.abstract_factory import abstract_factory
 from src.creational_patterns.builder.builder import builder_pattern
+from src.creational_patterns.factory_method.factory_method import factory_method
 
 
 def main():
-    factory_method()
+    abstract_factory()
     builder_pattern()
+    factory_method()
 
 
 if __name__ == "__main__":
--- src/creational_patterns/abstract_factory/abstract_factory.py
+++ src/creational_patterns/abstract_factory/abstract_factory.py
@@ -1,7 +1,8 @@
 from src.creational_patterns.abstract_factory.factory import ConcreteFactory1, ConcreteFactory2
 
 
-def factory_method():
+def abstract_factory():
+    print("Factory Method")
     for factory in (ConcreteFactory1(), ConcreteFactory2()):
         product_a = factory.create_product_a()
         product_b = factory.create_product_b()
